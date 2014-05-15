f8dba152ebb155e891dad396b09be6b385d11ef8
Jeswin <jeswinpk@agilehead.com>
2014-05-15 10:36:06 +0530

    Refactored test files. README.

--- test/test.js
+++ test/test.js
@@ -4,7 +4,9 @@
 describe('build.run()', function() {
     describe('when executed', function(){
         it('should run all configurations without throwing an exception', function(done){
-            require('./build')(done);
+            build = require('..').create({ threads: 4 });            
+            build.configure(require('./config'), 'data'); //data is the directory where the files are
+            build.start(false, done); //false indicates that the build need not monitor files for changes.
         });
     });
 
